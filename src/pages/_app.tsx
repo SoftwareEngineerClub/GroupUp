25b8e3ab98b1204b838183c33b1b6229bb22bc0c
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-06-11 21:27:49 -0500

    configure session state

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { SessionProvider } from "next-auth/react";
+import type { Session } from "next-auth";
 
 import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType<{ session: Session }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 };
 
 export default MyApp;
